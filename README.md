53d2ca7e63af4c82d8dcb0702777c09dff9cc243
lftimm <lftimm1902@gmail.com>
2022-01-23 19:00:39 -0300

    removido o urxvt

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # desktop-suckless
   Repositório pessoal. Contém minhas builds dos programas dmenu e dwm. Alguns patches foram aplicados.
 
-Detalhe: Utilizo o urxvtc e o fontawesome5 nessas builds. Consulte o gerenciador de pacotes de sua distribuição.
-
----
+--
 Lista de patches: **TBA**
--- meu-dwm/config.def.h
+++ meu-dwm/config.def.h
@@ -95,7 +95,7 @@ ResourcePref resources[] = {
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|Shiftmask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
